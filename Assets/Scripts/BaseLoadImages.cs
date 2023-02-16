a33f90a72b2874e70eaeffc5ceeedaf3a531df14
Marina <mbakanova2016@yandex.ru>
2023-02-16 19:13:13 +0300

    Start loading

--- Assets/Scripts/BaseLoadImages.cs
+++ Assets/Scripts/BaseLoadImages.cs
@@ -1,8 +1,30 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class BaseLoadImages : MonoBehaviour
 {
+    [SerializeField] 
+    private RawImage _cardImage;
+    private string _mediaUrl = "https://picsum.photos/200";
 
+    private void Start()
+    {
+        StartCoroutine(BaseLoadImage(_mediaUrl));
+    }
+    private IEnumerator BaseLoadImage(string _mediaUrl)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(_mediaUrl);
+        yield return request.SendWebRequest();
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(request.error);
+        }            
+        else
+        {
+            _cardImage.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+        }
+    } 
 }
